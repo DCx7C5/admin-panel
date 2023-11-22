a1ad6f2f7939cb69224bef72e232c16243f267f2
DCx7C5 <dcxdevelopment@protonmail.com>
2023-11-22 17:31:10 +0100

    Added missing auth urls and views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,14 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+    PasswordResetDoneView,
+    PasswordResetView,
+    PasswordChangeDoneView,
+)
+
 from django.urls import path
 
 from accounts.views import SignUpView
@@ -7,4 +17,10 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
