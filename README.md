b894b3de9560b24554824894d5c1d9916bf07453
DCx7C5 <dcxdevelopment@protonmail.com>
2023-11-22 15:50:57 +0100

    Added .env file handling

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # AsshatSuite
+
+#### How to start:
+
+###### Create .env file in project root:
+```shell
+$ echo -e 'DEBUG=True\nSECRET_KEY=' | tee .env
+```
+
+###### Create new secret:
+```shell
+$ python -c 'import secrets;print(secrets.token_urlsafe())'
+ACl_3BY4Uu-SECRET_KEY-p09cN1QgTY
+```
+###### Then add key to .env
--- asshatsuite/settings.py
+++ asshatsuite/settings.py
@@ -1,4 +1,10 @@
+from environs import Env
 from pathlib import Path
+
+"""Read environment variables"""
+env = Env()
+env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ij#dj0+d5!(taxn(#!6+m#2f)z8fpa1nrk1j4(p%ktk-8p05rr'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
