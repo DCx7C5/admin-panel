88376f62d2edc6ec6f75c33208b5397a53732c2b
DCx7C5 <dcxdevelopment@protonmail.com>
2023-11-22 13:37:44 +0100

    Added Admin Panel database tables

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.contrib.admin.models import LogEntry
+from django.contrib.sessions.models import Session
 
-# Register your models here.
+
+class SessionAdmin(ModelAdmin):
+    list_display = ('session_key', '_session_data', 'expire_date')
+
+
+class AdminLogAdmin(ModelAdmin):
+    list_display = ('action_time', 'user', 'content_type', 'change_message', 'is_addition', 'is_change', 'is_deletion')
+    list_filter = ['action_time', 'user', 'content_type']
+    ordering = ('-action_time',)
+
+
+admin.site.register(Session, SessionAdmin)
+admin.site.register(LogEntry, AdminLogAdmin)
