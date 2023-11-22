7d8f1345064386b59195f799facc81166a96c9de
DCx7C5 <dcxdevelopment@protonmail.com>
2023-11-22 17:30:24 +0100

    Dashboard only for logged-in users

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class DashboardView(TemplateView):
+class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/index.html'
