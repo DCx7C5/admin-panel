dbbca75203c32c65cae397206a747c226c42a52a
DCx7C5 <dcxdevelopment@protonmail.com>
2023-11-23 18:12:10 +0100

    Form updates

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,24 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.forms import (
-    Form,
-    CharField,
-    PasswordInput,
-)
-
-
-class LoginForm(Form):
-    username = CharField()
-    password = CharField(widget=PasswordInput)
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = (
-            "email",
-            "username",
-        )
+        fields = ('username', 'password1', 'password2')
 
 
 class CustomUserChangeForm(UserChangeForm):
