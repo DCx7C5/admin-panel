292d227ec0a3bf5f376ce0c5722cf76567ef0f46
DCx7C5 <dcxdevelopment@protonmail.com>
2023-12-09 09:07:35 +0100

    sidebar and terminal state handling

--- assets/js/core.jsx
+++ assets/js/core.jsx
@@ -1,9 +1,6 @@
 import '../css/core.css';
 import $ from "jquery/dist/jquery.slim";
 
-
-let sidebarState = localStorage.getItem('sbstate')
-
 function logout(url, csrfToken) {
   window.localStorage.clear();
   const form = document.createElement('form');
@@ -18,16 +15,52 @@ function logout(url, csrfToken) {
   form.submit();
 }
 
-function saveStateSidebar() {
 
+function sidebarStateHandler() {
+  let sbshow = localStorage.getItem('sb.show');
+  let hasshow = $("#sidebar").hasClass('show')
+  // Check if 'sbshow' is not set
+  if (sbshow === null) {
+    // Set 'sbshow' based on the class of #sidebar
+    localStorage.setItem('sb.show', hasshow);
+  }
+  // Check the value of sbshow and update #sidebar accordingly
+  if ((sbshow === 'true') && !hasshow) {
+    $('#sidebar').addClass('show');
+  } else if ((sbshow === 'false') && sbshow) {
+    $('#sidebar').removeClass('show');
+  }
+  // When sidebar button is clicked
+  $("#nav-sb-btn").on('click', function (e) {
+    let prevSbState = JSON.parse(localStorage.getItem('sb.show'));
+    // noinspection JSCheckFunctionSignatures
+    localStorage.setItem('sb.show', !prevSbState)
+  })
 }
 
-$(document).ready(function () {
-  console.log("dom ready");
-  let state = localStorage.getItem('sbstate') ? localStorage.setItem('sbstate', 'show') : 'show';
-
-  const nav_term_btn = document.getElementById('nav-term-btn');
-  nav_term_btn.on('click', function (e) {
-
+function termElemStateHandler() {
+  let termshow = localStorage.getItem('term.show');
+  let thasshow = $("#terminal-container").hasClass('show')
+  // Check if 'sbshow' is not set
+  if (termshow === null) {
+    // Set 'sbshow' based on the class of #sidebar
+    localStorage.setItem('term.show', thasshow);
+  }
+  // Check the value of sbshow and update #sidebar accordingly
+  if ((termshow === 'true') && !thasshow) {
+    $('#terminal-container').addClass('show');
+  } else if ((termshow === 'false') && termshow) {
+    $('#terminal-container').removeClass('show');
+  }
+  // When sidebar button is clicked
+  $("#nav-term-btn").on('click', function (e) {
+    let prevTermState = JSON.parse(localStorage.getItem('term.show'));
+    // noinspection JSCheckFunctionSignatures
+    localStorage.setItem('term.show', !prevTermState)
   })
+}
+
+$(document).ready(function() {
+  sidebarStateHandler();
+  termElemStateHandler();
 });
--- assets/js/terminal.jsx
+++ assets/js/terminal.jsx
@@ -1,33 +1,8 @@
-import { Terminal } from "xterm";
-import { FitAddon } from "xterm-addon-fit";
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
+
 import WebSocket from 'ws';
 import $ from 'jquery';
 
 const term_out_elem = document.getElementById('terminal-output');
-const term_container = document.getElementById('terminal-container');
-const term = new Terminal({
-    cursorBlink: true,
-});
-const fitAddon= new FitAddon();
-const socket = new WebSocket('ws://' + window.location.host + '/term/')
 
-function saveStateStorage() {
-
-}
-
-function loadStateStorage() {
-
-}
-
-function initTerminal() {
-    term.loadAddon(fitAddon);
-    term.open(term_out_elem);
-    fitAddon.fit();
-}
-
-function resizeTerminal() {
-    fitAddon.fit()
-    socket.emit('')
-}
-
-export { WebSocket, initTerminal, resizeTerminal };
